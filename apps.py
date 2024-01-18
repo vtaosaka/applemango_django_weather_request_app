ccfb4259ccf6be22dfdef569c2941facbbdb623f
apple <apple.mangoooo97@gmail.com>
2024-01-18 15:21:05 +0900

    move settings.py

--- apps.py
+++ apps.py
@@ -3,6 +3,23 @@ from django.urls import path
 from django.apps import AppConfig
 from requests import get
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = "django-insecure-0wo^wrqiz46dg1c+ua_+dvhl%q42pftg^+idp2g8y%n7-&-ihh"
+DEBUG = True
+ALLOWED_HOSTS = []
+INSTALLED_APPS = []
+ROOT_URLCONF = "apps"
+TEMPLATES = []
+TEMPLATE_DIRS = []
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = "static/"
+MIDDLEWARE = []
+
+
 
 def render(path: str, content_type: str = "text/html; charset=utf-8"):
     with open(path) as f:
